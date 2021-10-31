f4489cbec276894c9fe5913ac3e937fad5ed0472
tuuganbekov <48891404+tuuganbekov@users.noreply.github.com>
2021-10-31 17:47:01 +0600

    Add files via upload

---
+++ Lab_work_2/Lab_work_2/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_work_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int k = Convert.ToInt32((sender as Button).Tag);
+            string s = "You pressed the button";
+            switch (k)
+            {
+                case 1:
+                    s += "number 1";
+                    break;
+                case 2:
+                    s += "number 2";
+                    break;
+                default:
+                    s = "you couldn't get here";
+                    break;
+            }
+            MessageBox.Show(this, s, "Which button?");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i = 0; i < this.Controls.Count; i++)
+            {
+                if (Convert.ToString(this.Controls[i].GetType().Name) == "Button")
+                {
+                    MessageBox.Show(this, Convert.ToString(this.Controls[i].Name), "What buttons are there");
+                }
+            }
+        }
+    }
+}
