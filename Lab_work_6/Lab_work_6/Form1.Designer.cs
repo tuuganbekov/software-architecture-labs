1e39418bb27aba8d1c6eda6d93a608f6af9d065c
tuuganbekov <48891404+tuuganbekov@users.noreply.github.com>
2021-10-31 17:55:11 +0600

    Add files via upload

---
+++ Lab_work_6/Lab_work_6/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace Lab_work_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Items.AddRange(new object[] {
+            "First line",
+            "Second line",
+            "Third line",
+            "Fourth line",
+            "Fifth line",
+            "Sixth line"});
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(322, 319);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(351, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(293, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавление програмно строк";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(351, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(293, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Добавление из массива строк";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(351, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(293, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Добавляем строку";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(351, 103);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(293, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Функция AddRange";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(351, 133);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(293, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Выделение строк програмно";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(351, 163);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(293, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Сортировка";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(351, 193);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(293, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Свойство UseTabStop";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(351, 223);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(293, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Доступ к строке";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(351, 253);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(293, 23);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Перебор всех строк";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(351, 283);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(293, 23);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Удаление 1";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(351, 313);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(293, 23);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "Удаление 2";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 355);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ Lab_work_6/Lab_work_6/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_work_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for(int i = 1; i < 10; i++)
+            {
+                listBox1.Items.Add("Record " + i.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] sArrayItems = { "Excellent", "Good", "Satisfactory", "Unsatisfactory" };
+            listBox1.Items.Clear();
+            for(int i = 0; i < sArrayItems.GetLength(0); i++)
+            {
+                listBox1.Items.Add(sArrayItems[i]);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Insert(4, "Added line 2");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.AddRange(new object[] { "Excellent", "Good", "Satisfactory", "Unsatisfactory" });
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            for(int i = 1; i < 21; i++)
+            {
+                listBox1.Items.Add("Straw " + i.ToString());
+                listBox1.SelectionMode = SelectionMode.MultiSimple;
+                listBox1.SetSelected(0, true);
+                //listBox1.SetSelected(1, true);
+                //listBox1.SetSelected(2, true);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listBox1.Sorted = true;
+            listBox1.Items.Clear();
+            for(int i = 1; i < 21; i++)
+            {
+                listBox1.Items.Add("Straw " + i.ToString());
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox1.UseTabStops = true;
+            for (int i = 1; i < 21; i++)
+            {
+                listBox1.Items.Add("Option \t" + i.ToString());
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedIndex != -1)
+            {
+                MessageBox.Show(listBox1.SelectedItem.ToString(), "Geting the string value");
+                //MessageBox.Show(listBox1.Text, "");
+                if(listBox1.SelectedIndex != -1)
+                {
+                    MessageBox.Show(listBox1.SelectedIndex.ToString(), "Getting the string index");
+                }
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                MessageBox.Show(listBox1.Items[i].ToString() + "");
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.RemoveAt(0);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show(listBox1.SelectedItem.ToString(), "Getting string value");
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("We clicked on the component with the mouse");
+        }
+    }
+}
